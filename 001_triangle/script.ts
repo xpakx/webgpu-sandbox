e816acb1f842b05fca53dd8a2515ad889649d12c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-01 16:48:36 +0200

    feat: create simple shader generating single triangle

--- 001_triangle/script.ts
+++ 001_triangle/script.ts
@@ -27,4 +27,49 @@ document.addEventListener("DOMContentLoaded", async () => {
 	}
 	canvas.width = 800;
 	canvas.height = 640;
+
+	const context = canvas.getContext("webgpu");
+	if(!context) {
+		console.error("Cannot create WebGPU context");
+		return;
+	}
+
+	const format: GPUTextureFormat = "bgra8unorm";
+	context.configure({ device, format });
+
+
+	const shader = await fetch('./shader.wgsl')
+		.then((response) => response.text());
+
+	const pipeline =  device.createRenderPipeline(
+		{
+			vertex: {
+				module: device.createShaderModule({code: shader}),
+				entryPoint: "vs_main",
+			}, 
+			fragment: {
+				module: device.createShaderModule({code: shader}),
+				entryPoint: "fs_main",
+				targets: [{ format }],
+			}, 
+			primitive: { topology: "triangle-list" },
+			layout: "auto",
+		}
+	);
+
+	const commandEncode = device.createCommandEncoder();
+	const textureView = context.getCurrentTexture().createView();
+	const renderPass = commandEncode.beginRenderPass({
+		colorAttachments: [{
+			view: textureView,
+			clearValue: {r: 0.1176, g: 0.1176, b: 0.1804, a: 1.0},
+			loadOp: "clear",
+			storeOp: "store",
+
+		}],
+	});
+	renderPass.setPipeline(pipeline);
+	renderPass.draw(3, 1, 0, 0);
+	renderPass.end();
+	device.queue.submit([commandEncode.finish()]);
 });
