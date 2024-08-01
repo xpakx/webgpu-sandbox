cd8c89741100c09f4ff67daa4b3066cacbfa17d8
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-01 16:55:33 +0200

    feat: create two triangles filling the whole canvas

---
+++ 002_gradient/script.ts
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", async () => {
+	console.log("DOM fully loaded and parsed");
+
+	if (!navigator.gpu) {
+		console.error("WebGPU not supported.");
+		return;
+	}
+
+	console.log("Requesting adapter");
+	const adapter = await navigator.gpu.requestAdapter();
+	if (!adapter) {
+		console.error("No adapter");
+		return;
+	}
+
+	console.log("Requesting driver");
+	const device = await adapter.requestDevice();
+	if (!device) {
+		console.error("No device");
+		return;
+	}
+
+	const canvas = document.getElementById("canvas") as (HTMLCanvasElement | undefined);
+	if(!canvas) {
+		console.error("No canvas");
+		return;
+	}
+	canvas.width = 800;
+	canvas.height = 640;
+
+	const context = canvas.getContext("webgpu");
+	if(!context) {
+		console.error("Cannot create WebGPU context");
+		return;
+	}
+
+	const format: GPUTextureFormat = "bgra8unorm";
+	context.configure({ device, format });
+
+
+	const shader = await fetch('./shader.wgsl')
+		.then((response) => response.text());
+
+	const pipeline =  device.createRenderPipeline(
+		{
+			vertex: {
+				module: device.createShaderModule({code: shader}),
+				entryPoint: "vs_main",
+			}, 
+			fragment: {
+				module: device.createShaderModule({code: shader}),
+				entryPoint: "fs_main",
+				targets: [{ format }],
+			}, 
+			primitive: { topology: "triangle-list" },
+			layout: "auto",
+		}
+	);
+
+	const commandEncode = device.createCommandEncoder();
+	const textureView = context.getCurrentTexture().createView();
+	const renderPass = commandEncode.beginRenderPass({
+		colorAttachments: [{
+			view: textureView,
+			clearValue: {r: 0.1176, g: 0.1176, b: 0.1804, a: 1.0},
+			loadOp: "clear",
+			storeOp: "store",
+
+		}],
+	});
+	renderPass.setPipeline(pipeline);
+	renderPass.draw(6, 1, 0, 0);
+	renderPass.end();
+	device.queue.submit([commandEncode.finish()]);
+});
---
+++ 002_gradient/shader.wgsl
@@ -0,0 +1,35 @@
+struct Fragment {
+	@builtin(position) Position: vec4f,
+	@location(0) Color: vec4f,
+};
+
+@vertex
+fn vs_main(@builtin(vertex_index) v_id: u32) -> Fragment {
+	let positions = array<vec2f, 6> (
+			vec2f(-1.0, -1.0),
+			vec2f(1.0, -1.0),
+			vec2f(-1.0, 1.0),
+
+			vec2f(-1.0, 1.0),
+			vec2f(1.0, -1.0),
+			vec2f(1.0, 1.0)
+	);
+	let colors = array<vec3f, 6> (
+			vec3f(1.0, 0.0, 0.0),
+			vec3f(1.0, 0.0, 0.0),
+			vec3f(1.0, 0.0, 0.0),
+
+			vec3f(0.0, 1.0, 0.0),
+			vec3f(0.0, 1.0, 0.0),
+			vec3f(0.0, 1.0, 0.0),
+	);
+	var output: Fragment;
+	output.Position = vec4f(positions[v_id], 0.0, 1.0);
+	output.Color = vec4f(colors[v_id], 1.0);
+	return output;
+}
+
+@fragment
+fn fs_main(@location(0) Color: vec4f) -> @location(0) vec4f {
+	return Color;
+}
